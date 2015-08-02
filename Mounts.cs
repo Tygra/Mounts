f4019f9d029a977af35abd7592549ee0e4db974d
Hiarni <stefan.schaeflinger@gmail.com>
2015-08-02 19:07:28 +0200

    Revert "Revert "removed capital letters""
    This reverts commit 0019357f3538726ad22d519631ce60b18651e74f.

--- Mounts.cs
+++ Mounts.cs
@@ -67,55 +67,55 @@ namespace Mounts
 
         private void OnInitialize(EventArgs args)
         {
-            Commands.ChatCommands.Add(new Command("mounts.rudolph", MRUDOLPH, "rudolph"));
-            Commands.ChatCommands.Add(new Command("mounts.bunny", MBUNNY, "bunny"));
-            Commands.ChatCommands.Add(new Command("mounts.pigron", MPIGRON, "pigron"));
-            Commands.ChatCommands.Add(new Command("mounts.slime", MSLIME, "slime"));
-            Commands.ChatCommands.Add(new Command("mounts.turtle", MTURTLE, "turtle"));
-            Commands.ChatCommands.Add(new Command("mounts.bee", MBEE, "bee"));
+            Commands.ChatCommands.Add(new Command("mounts.rudolph", MRudolph, "rudolph"));
+            Commands.ChatCommands.Add(new Command("mounts.bunny", MBunny, "bunny"));
+            Commands.ChatCommands.Add(new Command("mounts.pigron", MPigron, "pigron"));
+            Commands.ChatCommands.Add(new Command("mounts.slime", MSlime, "slime"));
+            Commands.ChatCommands.Add(new Command("mounts.turtle", MTurtle, "turtle"));
+            Commands.ChatCommands.Add(new Command("mounts.bee", MBee, "bee"));
             Commands.ChatCommands.Add(new Command("mounts.minecart", MMC, "minecart"));
             Commands.ChatCommands.Add(new Command("mounts.ufo", MUFO, "ufo"));
-            Commands.ChatCommands.Add(new Command("mounts.drill", MDRILL, "drill"));
-            Commands.ChatCommands.Add(new Command("mounts.scutlix", MSCUTLIX, "scutlix"));
-            Commands.ChatCommands.Add(new Command("mounts.unicorn", MUNICORN, "unicorn"));
-            Commands.ChatCommands.Add(new Command("mounts.fishron", MFISHRON, "fishron"));
+            Commands.ChatCommands.Add(new Command("mounts.drill", MDrill, "drill"));
+            Commands.ChatCommands.Add(new Command("mounts.scutlix", MScutlix, "scutlix"));
+            Commands.ChatCommands.Add(new Command("mounts.unicorn", MUnicorn, "unicorn"));
+            Commands.ChatCommands.Add(new Command("mounts.fishron", MFishron, "fishron"));
         }
 
 
-        private void MRUDOLPH(CommandArgs args)
+        private void MRudolph(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(90);
                 args.Player.SendInfoMessage("You summoned a rideable Reindeer!");
             }
 
-        private void MBUNNY(CommandArgs args)
+        private void MBunny(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(128);
             args.Player.SendInfoMessage("You summoned a rideable Bunny!");
         }
 
-        private void MPIGRON(CommandArgs args)
+        private void MPigron(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(129);
             args.Player.SendInfoMessage("You summoned a rideable Pig! Oink Oink!");
         }
 
-        private void MSLIME(CommandArgs args)
+        private void MSlime(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(130);
             args.Player.SendInfoMessage("You summoned a rideable Slime! How slippy!");
         }
-        private void MTURTLE(CommandArgs args)
+        private void MTurtle(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(131);
             args.Player.SendInfoMessage("You summoned a rideable Turtle! Come on... move on!");
         }
-        private void MBEE(CommandArgs args)
+        private void MBee(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(132);
@@ -133,25 +133,25 @@ namespace Mounts
             args.Player.SetBuff(141);
             args.Player.SendInfoMessage("You summoned a flying UFO! Eeeeeeeeeteeee phone home!");
         }
-        private void MDRILL(CommandArgs args)
+        private void MDrill(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(142);
             args.Player.SendInfoMessage("You summoned a rideable Drill! What the heck?!");
         }
-        private void MSCUTLIX(CommandArgs args)
+        private void MScutlix(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(143);
             args.Player.SendInfoMessage("You summoned a rideable Scutlix creature!");
         }
-        private void MUNICORN(CommandArgs args)
+        private void MUnicorn(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(162);
             args.Player.SendInfoMessage("You summoned a rideable Unicorn! Don't cut the horn!");
         }
-        private void MFISHRON(CommandArgs args)
+        private void MFishron(CommandArgs args)
         {
             TSPlayer ply = args.Player;
             args.Player.SetBuff(168);
